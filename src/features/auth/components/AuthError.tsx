9aaf0c0a5e74a3edab20578faf232aad3890a89a
SandipBajracharya <sndp106.sb@gmail.com>
2025-10-14 19:36:50 +0545

    refactor(OUT-2469): replace window open by next navigation for auth retry

--- src/features/auth/components/AuthError.tsx
+++ src/features/auth/components/AuthError.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { SilentError } from '@/components/layouts/SilentError'
 import { authInitUrl } from './Callout'
 
 export const AuthError = ({ error, token }: { error: string; token: string }) => {
+  const router = useRouter()
+
   return (
     <div className="flex items-center justify-center px-2 py-4">
-      <SilentError message={error} resetFn={() => window.open(`${authInitUrl}${token}`, '_self')} />
+      <SilentError message={error} resetFn={() => router.push(authInitUrl + token)} />
     </div>
   )
 }
