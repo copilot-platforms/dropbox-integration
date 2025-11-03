ccb5ca844427a9d1f02a746c3b3cfbfed6289496
SandipBajracharya <sndp106.sb@gmail.com>
2025-11-03 14:39:02 +0545

    feat(OUT-2493): sync existing files/folders sync from Assembly to Dropbox
    - [x] sync all files and folders from Assembly to Dropbox
    - [x] changed flow to sync Assembly to Dropbox first.
    - [x] rename existing file in Dropbox if the uploading filename is same
    - [x] decrease the file limit to make the batch size small

--- src/features/sync/lib/Sync.service.ts
+++ src/features/sync/lib/Sync.service.ts
@@ -1,21 +1,21 @@
 import { and, eq } from 'drizzle-orm'
+import { type Dropbox, DropboxResponseError } from 'dropbox'
 import { ApiError } from 'node_modules/copilot-node-sdk/dist/codegen/api'
 import { ObjectType, type ObjectTypeValue } from '@/db/constants'
 import type { DropboxConnectionTokens } from '@/db/schema/dropboxConnections.schema'
 import { type FileSyncCreateType, fileFolderSync } from '@/db/schema/fileFolderSync.schema'
+import { getFetcher } from '@/helper/fetcher.helper'
 import { CopilotAPI } from '@/lib/copilot/CopilotAPI'
 import type User from '@/lib/copilot/models/User.model'
+import type { CopilotFileRetrieve } from '@/lib/copilot/types'
 import AuthenticatedDropboxService from '@/lib/dropbox/AuthenticatedDropbox.service'
-import { DropboxApi } from '@/lib/dropbox/DropboxApi'
-import { processDropboxSyncToAssemblyTask } from '@/trigger/processFileSync'
-import { buildPathArray } from '@/utils/filePath'
-import { MAX_FILES_LIMIT } from '../constant'
-import {
-  DropboxFileListFolderResultEntriesSchema,
-  type DropboxFileListFolderSingleEntry,
-  type DropboxToAssemblySyncFilesPayload,
-  type DropboxToAssemblySyncTaskPayload,
-  type WhereClause,
+import { bidirectionalMasterSync } from '@/trigger/processFileSync'
+import { appendDateTimeToFilePath, buildPathArray } from '@/utils/filePath'
+import type {
+  AssemblyToDropboxSyncFilesPayload,
+  DropboxFileListFolderSingleEntry,
+  DropboxToAssemblySyncFilesPayload,
+  WhereClause,
 } from '../types'
 import { MapFilesService } from './MapFiles.service'
 
@@ -28,74 +28,16 @@ export class SyncService extends AuthenticatedDropboxService {
   }
 
   async initiateSync(assemblyChannelId: string) {
-    // 1. expect assembly channel and dropbox folder path Inputs.
+    // expect assembly channel and dropbox folder path Inputs.
     const dbxRootPath = '/Assembly'
 
-    // 2. sync dropbox folder to assembly channel
-    await this.processDropboxSyncToAssembly(dbxRootPath, assemblyChannelId)
-
-    // 3. TODO: sync assembly channel to dropbox folder
-  }
-
-  private async processDropboxSyncToAssembly(dbxRootPath: string, assemblyChannelId: string) {
-    // 1. store channel sync
-    const channelPayload = {
-      dbxAccountId: this.connectionToken.accountId,
-      assemblyChannelId,
+    // bidrectional sync
+    await bidirectionalMasterSync.trigger({
       dbxRootPath,
-    }
-    const channelMap = await this.mapFilesService.getOrCreateChannelMap(channelPayload)
-
-    // 1. get all the files folder from dropbox
-    const dbxApi = new DropboxApi()
-    const dbxClient = dbxApi.getDropboxClient(this.connectionToken.refreshToken)
-
-    let dbxFiles = await dbxClient.filesListFolder({
-      path: dbxRootPath,
-      recursive: true,
-      limit: MAX_FILES_LIMIT,
+      assemblyChannelId,
+      connectionToken: this.connectionToken,
+      user: this.user,
     })
-    let loopOver = !!dbxFiles.result.entries.length
-
-    this.dbxApi.refreshAccessToken(this.connectionToken.refreshToken)
-
-    while (loopOver) {
-      const parsedDbxFiles = DropboxFileListFolderResultEntriesSchema.safeParse(
-        dbxFiles.result.entries,
-      )
-
-      if (!parsedDbxFiles.success) {
-        console.error('Error parsing Dropbox files', parsedDbxFiles.error)
-        break
-      }
-
-      const parsedDbxEntries = parsedDbxFiles.data
-      const syncTaskPayload: DropboxToAssemblySyncTaskPayload = {
-        resultEntries: parsedDbxEntries,
-        dbxRootPath,
-        assemblyChannelId,
-        channelSyncId: channelMap.id,
-        user: this.user,
-        connectionToken: this.connectionToken,
-      }
-
-      await processDropboxSyncToAssemblyTask.trigger(syncTaskPayload)
-
-      if (!dbxFiles.result.has_more || !parsedDbxEntries.length) {
-        loopOver = false
-
-        // update channelSync with lastest cursor
-        await this.mapFilesService.updateChannelMap({
-          dbxCursor: dbxFiles.result.cursor,
-        })
-        break
-      }
-
-      // continue pagination
-      dbxFiles = await dbxClient.filesListFolderContinue({
-        cursor: dbxFiles.result.cursor,
-      })
-    }
   }
 
   async syncDropboxFilesToAssembly({ entry, opts }: DropboxToAssemblySyncFilesPayload) {
@@ -108,7 +50,6 @@ export class SyncService extends AuthenticatedDropboxService {
 
     for (let i = 0; i < pathArray.length; i++) {
       const lastItem = i === pathArray.length - 1
-
       const itemPath = pathArray[i]
 
       await this.createAndUploadFileToAssembly(
@@ -181,7 +122,6 @@ export class SyncService extends AuthenticatedDropboxService {
         )
         return
       }
-      console.error(error)
       throw error
     }
   }
@@ -211,5 +151,85 @@ export class SyncService extends AuthenticatedDropboxService {
         fileMapCondition,
       )
     }
+  }
+
+  async syncAssemblyFilesToDropbox({ file, opts }: AssemblyToDropboxSyncFilesPayload) {
+    const { channelSyncId, dbxRootPath } = opts
+    const dbxFileInfo = await this.createAndUploadFileInDropbox(dbxRootPath, file.object, file)
+
+    const filePayload = {
+      channelSyncId,
+      itemPath: file.path,
+      object: file.object,
+      portalId: this.user.portalId,
+      assemblyFileId: file.id,
+    }
+    await this.mapFilesService.insertFileMap({
+      ...filePayload,
+      ...dbxFileInfo,
+    })
+  }
+
+  async createAndUploadFileInDropbox(
+    dbxRootPath: string,
+    fileType: ObjectTypeValue,
+    file: CopilotFileRetrieve,
+  ): Promise<{ dbxFileId: string; contentHash?: string } | undefined> {
+    const dbxClient = this.dbxApi.getDropboxClient(this.connectionToken.refreshToken)
+    const dbxFilePath = `${dbxRootPath}/${file.path}`
+
+    // create file/folder
+    try {
+      // 1. check if the file/folder exists
+      const dbxResponse = await dbxClient.filesGetMetadata({
+        path: dbxFilePath,
+      })
+      // 1.1 if folder exists, simply return the folder id
+      if (dbxResponse.result['.tag'] === ObjectType.FOLDER) {
+        return { dbxFileId: dbxResponse.result.id }
+      } else if (dbxResponse.result['.tag'] === ObjectType.FILE) {
+        // 1.2 if file exists, rename the existing file in Dropbox and create a new file
+        const newFilePath = appendDateTimeToFilePath(dbxFilePath)
+
+        await dbxClient.filesMoveV2({
+          from_path: dbxFilePath,
+          to_path: newFilePath,
+        })
+
+        return await this.uploadFileInDropbox(file, dbxFilePath, dbxClient)
+      }
+    } catch (error: unknown) {
+      // 2. if doesn't exist, create the file/folder
+      if (
+        error instanceof DropboxResponseError &&
+        error.status === 409 &&
+        error.error.error.path['.tag'] === 'not_found'
+      ) {
+        if (fileType === ObjectType.FOLDER) {
+          const folderCreateResponse = await dbxClient.filesCreateFolderV2({
+            path: dbxFilePath,
+          })
+          return { dbxFileId: folderCreateResponse.result.metadata.id }
+        } else if (fileType === ObjectType.FILE) {
+          return await this.uploadFileInDropbox(file, dbxFilePath, dbxClient)
+        }
+      }
+      throw error
+    }
+  }
+
+  private async uploadFileInDropbox(file: CopilotFileRetrieve, path: string, dbxClient: Dropbox) {
+    if (file.downloadUrl) {
+      // download file from Assembly
+      const resp = await getFetcher(file.downloadUrl)
+      const buffer = await resp.arrayBuffer()
+      // upload file to dropbox
+      const dbxResponse = await dbxClient.filesUpload({
+        contents: buffer,
+        path,
+      })
+      return { dbxFileId: dbxResponse.result.id, contentHash: dbxResponse.result.content_hash }
+    }
+    throw new Error('File not found')
   }
 }
--- src/trigger/processFileSync.ts
+++ src/trigger/processFileSync.ts
@@ -1,59 +1,92 @@
 import { task } from '@trigger.dev/sdk/v3'
-import { ObjectType } from '@/db/constants'
+import type { DropboxConnectionTokens } from '@/db/schema/dropboxConnections.schema'
+import { MAX_FILES_LIMIT } from '@/features/sync/constant'
 import { MapFilesService } from '@/features/sync/lib/MapFiles.service'
 import { SyncService } from '@/features/sync/lib/Sync.service'
-import type {
-  DropboxToAssemblySyncFilesPayload,
-  DropboxToAssemblySyncTaskPayload,
+import {
+  type AssemblyToDropboxSyncFilesPayload,
+  DropboxFileListFolderResultEntriesSchema,
+  type DropboxToAssemblySyncFilesPayload,
 } from '@/features/sync/types'
+import { CopilotAPI } from '@/lib/copilot/CopilotAPI'
+import type User from '@/lib/copilot/models/User.model'
+import { DropboxApi } from '@/lib/dropbox/DropboxApi'
 
-export const processDropboxSyncToAssemblyTask = task({
-  id: 'process-dropbox-sync-to-assembly',
-  run: async (payload: DropboxToAssemblySyncTaskPayload) => {
-    const { resultEntries, ...restPayload } = payload
+type SyncTaskPayload = {
+  dbxRootPath: string
+  assemblyChannelId: string
+  connectionToken: DropboxConnectionTokens
+  user: User
+}
 
-    const mapFilesService = new MapFilesService(restPayload.user, restPayload.connectionToken)
-
-    const mappedEntries = await Promise.all(
-      resultEntries.map(async (entry) => {
-        const fileObjectType = entry['.tag']
-        if (
-          (fileObjectType === ObjectType.FOLDER &&
-            entry.path_display !== restPayload.dbxRootPath) ||
-          fileObjectType === ObjectType.FILE
-        ) {
-          const basePath = entry.path_display.replace(restPayload.dbxRootPath, '') // removes the base folder path
-          const isMapped = await mapFilesService.getMappedDbxFile(
-            basePath,
-            entry.id,
-            restPayload.channelSyncId,
-          )
-          if (!isMapped) return { opts: restPayload, entry }
-
-          return null
-        }
-      }),
-    )
-    const filteredEntries = mappedEntries.filter((entry) => !!entry)
-
-    if (!filteredEntries.length) {
-      console.info('No new files to sync')
-      return
-    }
-
-    await syncDropboxFileToAssemblyTask.batchTrigger(
-      filteredEntries.map((entry) => ({
-        payload: entry,
-      })),
-    )
+export const bidirectionalMasterSync = task({
+  id: 'bidirectional-master-sync',
+  run: async (payload: SyncTaskPayload) => {
+    await processAssemblyToDropboxSync.triggerAndWait(payload)
+    console.info('\n\n Synced Assembly files to Dropbox \n\n')
+    await processDropboxToAssemblySync.trigger(payload)
   },
 })
 
-export const syncDropboxFileToAssemblyTask = task({
+export const processDropboxToAssemblySync = task({
+  id: 'process-dropbox-sync-to-assembly',
+  run: async (payload: SyncTaskPayload) => {
+    const { dbxRootPath, assemblyChannelId, connectionToken, user } = payload
+    const mapFilesService = new MapFilesService(user, connectionToken)
+
+    // 1. get all the files folder from dropbox
+    const dbxApi = new DropboxApi()
+    dbxApi.refreshAccessToken(connectionToken.refreshToken)
+    const dbxClient = dbxApi.getDropboxClient(connectionToken.refreshToken)
+
+    let dbxFiles = await dbxClient.filesListFolder({
+      path: dbxRootPath,
+      recursive: true,
+      limit: MAX_FILES_LIMIT,
+    })
+
+    // 2. loop over the dropbox files
+    while (dbxFiles.result.entries.length) {
+      const parsedDbxFiles = DropboxFileListFolderResultEntriesSchema.safeParse(
+        dbxFiles.result.entries,
+      )
+
+      if (!parsedDbxFiles.success) {
+        console.error('Error parsing Dropbox files', parsedDbxFiles.error)
+        break
+      }
+      const parsedDbxEntries = parsedDbxFiles.data
+
+      // check and filter out all the mapped files
+      const filteredEntries = await mapFilesService.checkAndFilterDbxFiles(
+        parsedDbxEntries,
+        dbxRootPath,
+        assemblyChannelId,
+      )
+
+      if (filteredEntries.length) await syncDropboxFileToAssembly.batchTrigger(filteredEntries)
+
+      if (!dbxFiles.result.has_more) {
+        // update channelSync with lastest cursor
+        await mapFilesService.updateChannelMap({
+          dbxCursor: dbxFiles.result.cursor,
+        })
+        break
+      }
+
+      // continue pagination
+      dbxFiles = await dbxClient.filesListFolderContinue({
+        cursor: dbxFiles.result.cursor,
+      })
+    }
+  },
+})
+
+export const syncDropboxFileToAssembly = task({
   id: 'sync-dropbox-file-to-assembly',
   queue: {
     name: 'sync-dropbox-file-to-assembly',
-    concurrencyLimit: 20,
+    concurrencyLimit: 5,
   },
   retry: {
     maxAttempts: 3,
@@ -62,5 +95,67 @@ export const syncDropboxFileToAssemblyTask = task({
     const { opts, entry } = payload
     const syncService = new SyncService(opts.user, opts.connectionToken)
     await syncService.syncDropboxFilesToAssembly({ entry, opts })
+  },
+})
+
+export const processAssemblyToDropboxSync = task({
+  id: 'process-assembly-sync-to-dropbox',
+  run: async (payload: SyncTaskPayload) => {
+    const { user, connectionToken, dbxRootPath, assemblyChannelId } = payload
+    const mapFilesService = new MapFilesService(user, connectionToken)
+
+    // refresh dropbox access token
+    const dbxApi = new DropboxApi()
+    dbxApi.refreshAccessToken(connectionToken.refreshToken)
+
+    // 1. get al the files from the assembly
+    const copilotApi = new CopilotAPI(payload.user.token)
+    let files = await copilotApi.listFiles(payload.assemblyChannelId)
+
+    // biome-ignore lint/suspicious/noExplicitAny: just for awaiting purpose so its safe to ignore
+    const batchPromises: Promise<any>[] = []
+
+    // TODO: implement bottleneck for copilot sdk
+    while (files.data.length) {
+      // 2. check and filter out all the mapped files
+      const filteredEntries = await mapFilesService.checkAndFilterAssemblyFiles(
+        files.data,
+        dbxRootPath,
+        assemblyChannelId,
+      )
+
+      if (filteredEntries.length) {
+        // TODO: task to sync the files to dropbox
+        const batchPromise = syncAssemblyFileToDropbox.batchTrigger(filteredEntries)
+        batchPromises.push(batchPromise)
+      }
+
+      if (!files.nextToken) {
+        break
+      }
+
+      files = await copilotApi.listFiles(payload.assemblyChannelId, files.nextToken)
+    }
+    /**
+     * 3. await all the batch promises.
+     * This is required to ensure that all the files are synced to assembly first before syncing assembly to dropbox
+     * */
+    await Promise.all(batchPromises)
+  },
+})
+
+export const syncAssemblyFileToDropbox = task({
+  id: 'sync-assembly-file-to-dropbox',
+  queue: {
+    name: 'sync-assembly-file-to-dropbox',
+    concurrencyLimit: 5,
+  },
+  retry: {
+    maxAttempts: 3,
+  },
+  run: async (payload: AssemblyToDropboxSyncFilesPayload) => {
+    const { opts, file } = payload
+    const syncService = new SyncService(opts.user, opts.connectionToken)
+    await syncService.syncAssemblyFilesToDropbox({ file, opts })
   },
 })
