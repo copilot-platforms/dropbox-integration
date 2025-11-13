1300c419e61ff9ba439f5de65387a79857c2fdfb
arpandhakal <arpandhakal20@gmail.com>
2025-11-13 18:21:29 +0545

    feat(OUT-2596): finalized dropbox --> aseembly sync using webhook. Refactored code

--- src/features/sync/lib/MapFiles.service.ts
+++ src/features/sync/lib/MapFiles.service.ts
@@ -46,6 +46,10 @@ export class MapFilesService extends AuthenticatedDropboxService {
     return mappedFile
   }
 
+  async deleteFileMap(id: string): Promise<void> {
+    await db.delete(fileFolderSync).where(eq(fileFolderSync.id, id))
+  }
+
   async updateFileMap(
     payload: FileSyncUpdatePayload,
     condition: WhereClause,
@@ -63,6 +67,17 @@ export class MapFilesService extends AuthenticatedDropboxService {
       and(
         eq(fileFolderSync.channelSyncId, channelSyncId),
         eq(fileFolderSync.dbxFileId, dbxId),
+        isNotNull(fileFolderSync.assemblyFileId),
+      ) as WhereClause,
+    )
+    return mappedFile
+  }
+
+  async getDbxMappedFileFromPath(dbxPath: string, channelSyncId: string) {
+    const [mappedFile] = await this.getAllFileMaps(
+      and(
+        eq(fileFolderSync.channelSyncId, channelSyncId),
+        eq(fileFolderSync.itemPath, dbxPath),
         isNotNull(fileFolderSync.assemblyFileId),
       ) as WhereClause,
     )
---
+++ src/features/webhook/dropbox/utils/getDropboxChanges.ts
@@ -0,0 +1,58 @@
+import type { MapFilesService } from '@/features/sync/lib/MapFiles.service'
+import { DropboxFileListFolderResultEntriesSchema } from '@/features/sync/types'
+import type { DropboxApi } from '@/lib/dropbox/DropboxApi'
+
+export async function getDropboxChanges(
+  refreshToken: string,
+  cursor: string,
+  rootPath: string,
+  dbxApi: DropboxApi,
+  mapFilesService: MapFilesService,
+  channelSyncId: string,
+) {
+  dbxApi.refreshAccessToken(refreshToken)
+  const dbx = dbxApi.getDropboxClient(refreshToken)
+
+  try {
+    const response = await dbx.filesListFolderContinue({ cursor })
+
+    const entriesWithId = (
+      await Promise.all(
+        response.result.entries.map(async (entry) => {
+          if (entry['.tag'] === 'deleted') {
+            console.info('ENTRY HERE', entry)
+            const basePath = entry.path_display?.replace(rootPath, '')
+
+            const mappedFile = await mapFilesService.getDbxMappedFileFromPath(
+              basePath ?? '',
+              channelSyncId,
+            )
+            if (!mappedFile) return null
+            return { ...entry, id: mappedFile.dbxFileId }
+          }
+          return entry
+        }),
+      )
+    ).filter(Boolean)
+
+    const parsed = DropboxFileListFolderResultEntriesSchema.safeParse(entriesWithId)
+
+    if (!parsed.success) {
+      console.error('Invalid Dropbox response entries:', parsed.error)
+      throw new Error('Invalid Dropbox entries format')
+    }
+
+    const filtered = parsed.data.filter((entry) =>
+      entry.path_display.toLowerCase().startsWith(rootPath.toLowerCase()),
+    )
+
+    return {
+      entries: filtered,
+      newCursor: response.result.cursor,
+      hasMore: response.result.has_more,
+    }
+  } catch (error) {
+    console.error('Error fetching Dropbox changes:', error)
+    throw error
+  }
+}
