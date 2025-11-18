4eeb835bd8440ab85901872d3e6cc7ef24a7f13f
arpandhakal <arpandhakal20@gmail.com>
2025-11-18 19:24:02 +0545

    fix(OUT-2495): removed unused migrations

--- src/db/migrations/20251117121042_shocking_patriot.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "channel_sync" ALTER COLUMN "status" SET NOT NULL;
-ALTER TABLE "file_folder_sync" ADD COLUMN "synced_from_assembly" boolean DEFAULT false NOT NULL;
-ALTER TABLE "file_folder_sync" ADD COLUMN "synced_from_dropbox" boolean DEFAULT false NOT NULL;
--- src/db/migrations/20251118071509_spooky_sunset_bain.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "file_folder_sync" DROP COLUMN "synced_from_assembly";
-ALTER TABLE "file_folder_sync" DROP COLUMN "synced_from_dropbox";
