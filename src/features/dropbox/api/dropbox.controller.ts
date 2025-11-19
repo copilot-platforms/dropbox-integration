8bfa283bed6c0634aaecb6b92e2ce28e9f786cf6
SandipBajracharya <sndp106.sb@gmail.com>
2025-11-19 17:11:36 +0545

    fix(OUT-2605): throw APIError from the api controller when error

--- src/features/dropbox/api/dropbox.controller.ts
+++ src/features/dropbox/api/dropbox.controller.ts
@@ -1,4 +1,6 @@
+import httpStatus from 'http-status'
 import { type NextRequest, NextResponse } from 'next/server'
+import APIError from '@/errors/APIError'
 import DropboxConnectionsService from '@/features/auth/lib/DropboxConnections.service'
 import { DropboxService } from '@/features/dropbox/lib/Dropbox.service'
 import User from '@/lib/copilot/models/User.model'
@@ -10,8 +12,8 @@ export const getFolderTree = async (req: NextRequest) => {
   const dbxService = new DropboxConnectionsService(user)
   const connection = await dbxService.getConnectionForWorkspace()
 
-  if (!connection.refreshToken) throw new Error('No refresh token found')
-  if (!connection.accountId) throw new Error('No accountId found')
+  if (!connection.refreshToken) throw new APIError('No refresh token found', httpStatus.NOT_FOUND)
+  if (!connection.accountId) throw new APIError('No account Id found', httpStatus.NOT_FOUND)
 
   const dropboxService = new DropboxService(user, {
     refreshToken: connection.refreshToken,
