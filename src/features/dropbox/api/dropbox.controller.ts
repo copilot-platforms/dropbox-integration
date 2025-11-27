d674c40c320c36bb1d5e43996b89353cd636ba2e
Rojan Rajbhandari <rrojan20@gmail.com>
2025-11-27 20:39:36 +0545

    hotfix: force folder tree to be an empty array

--- src/features/dropbox/api/dropbox.controller.ts
+++ src/features/dropbox/api/dropbox.controller.ts
@@ -6,19 +6,24 @@ import { DropboxService } from '@/features/dropbox/lib/Dropbox.service'
 import User from '@/lib/copilot/models/User.model'
 
 export const getFolderTree = async (req: NextRequest) => {
-  const token = req.nextUrl.searchParams.get('token')
+  try {
+    const token = req.nextUrl.searchParams.get('token')
 
-  const user = await User.authenticate(token)
-  const dbxService = new DropboxConnectionsService(user)
-  const connection = await dbxService.getConnectionForWorkspace()
+    const user = await User.authenticate(token)
+    const dbxService = new DropboxConnectionsService(user)
+    const connection = await dbxService.getConnectionForWorkspace()
 
-  if (!connection.refreshToken) throw new APIError('No refresh token found', httpStatus.NOT_FOUND)
-  if (!connection.accountId) throw new APIError('No account Id found', httpStatus.NOT_FOUND)
+    if (!connection.refreshToken) throw new APIError('No refresh token found', httpStatus.NOT_FOUND)
+    if (!connection.accountId) throw new APIError('No account Id found', httpStatus.NOT_FOUND)
 
-  const dropboxService = new DropboxService(user, {
-    refreshToken: connection.refreshToken,
-    accountId: connection.accountId,
-  })
-  const folders = await dropboxService.getFolderTree()
-  return NextResponse.json({ message: 'Sync initiated successfully', folders }, { status: 200 })
+    const dropboxService = new DropboxService(user, {
+      refreshToken: connection.refreshToken,
+      accountId: connection.accountId,
+    })
+    const folders = await dropboxService.getFolderTree()
+    return NextResponse.json({ message: 'Sync initiated successfully', folders }, { status: 200 })
+  } catch (error) {
+    console.warn('Something went wrong', error)
+    return NextResponse.json({ message: 'Something went wrong', folders: [] })
+  }
 }
