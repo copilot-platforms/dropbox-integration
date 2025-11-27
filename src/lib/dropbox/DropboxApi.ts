12a892900c2efc2cab05dfa43dcca6e6c1c12300
Rojan Rajbhandari <rrojan20@gmail.com>
2025-11-27 14:02:45 +0545

    feat(OUT-2689): use retry logic for Dropbox API / SDK calls

--- src/lib/dropbox/DropboxApi.ts
+++ src/lib/dropbox/DropboxApi.ts
@@ -9,6 +9,7 @@ import {
   type DropboxFileMetadata,
   DropboxFileMetadataSchema,
 } from '@/lib/dropbox/type'
+import { withRetry } from '@/lib/withRetry'
 import { dropboxArgHeader } from '@/utils/header'
 
 export class DropboxApi {
@@ -56,7 +57,7 @@ export class DropboxApi {
     return new Dropbox({ auth: this.dropboxAuth })
   }
 
-  async getAuthUrl(state: string) {
+  async _getAuthUrl(state: string) {
     return await this.dropboxAuth.getAuthenticationUrl(
       env.DROPBOX_REDIRECT_URI,
       state,
@@ -68,7 +69,7 @@ export class DropboxApi {
     )
   }
 
-  async handleDropboxCallback(
+  async _handleDropboxCallback(
     urlParams: Record<string, string | string[] | undefined>,
   ): Promise<DropboxAuthResponseType> {
     const code = urlParams.code
@@ -83,12 +84,12 @@ export class DropboxApi {
     return DropboxAuthResponseSchema.parse(camelKeys(tokenSet.result))
   }
 
-  async downloadFile(urlPath: string, filePath: string) {
+  async _downloadFile(urlPath: string, filePath: string) {
     const headers = {
       Authorization: `Bearer ${this.dropboxAuth.getAccessToken()}`,
       'Dropbox-API-Arg': dropboxArgHeader({ path: filePath }),
     }
-    const response = await this._manualFetch(`${env.DROPBOX_API_URL}${urlPath}`, headers)
+    const response = await this.manualFetch(`${env.DROPBOX_API_URL}${urlPath}`, headers)
     if (response.status !== httpStatus.OK)
       throw new Error('DropboxApi#downloadFile. Failed to download file')
     return response.body
@@ -98,7 +99,7 @@ export class DropboxApi {
    * Description: this function streams the file to Dropbox. @param body is the readable stream of the file.
    * For the stream to work we need to add the Content-Type: 'application/octet-stream' in the headers.
    */
-  async uploadFile(
+  async _uploadFile(
     urlPath: string,
     filePath: string,
     body: NodeJS.ReadableStream | null,
@@ -113,9 +114,26 @@ export class DropboxApi {
       'Dropbox-API-Arg': dropboxArgHeader(args),
       'Content-Type': 'application/octet-stream',
     }
-    const response = await this._manualFetch(`${env.DROPBOX_API_URL}${urlPath}`, headers, body)
+    const response = await this.manualFetch(`${env.DROPBOX_API_URL}${urlPath}`, headers, body)
     if (response.status !== httpStatus.OK)
       throw new Error('DropboxApi#uploadFile. Failed to upload file')
     return DropboxFileMetadataSchema.parse(camelKeys(await response.json()))
   }
+
+  private wrapWithRetry<Args extends unknown[], R>(
+    fn: (...args: Args) => Promise<R>,
+  ): (...args: Args) => Promise<R> {
+    return (...args: Args): Promise<R> =>
+      withRetry(fn.bind(this), args, {
+        minTimeout: 3000,
+        // After a facter 2 exponential backoff [minTimeout * factor^(attemptNumber - 1)] timeout after 3 retries is 12 secs
+        maxTimeout: 12000,
+      })
+  }
+
+  manualFetch = this.wrapWithRetry(this._manualFetch)
+  getAuthUrl = this.wrapWithRetry(this._getAuthUrl)
+  handleDropboxCallback = this.wrapWithRetry(this._handleDropboxCallback)
+  downloadFile = this.wrapWithRetry(this._downloadFile)
+  uploadFile = this.wrapWithRetry(this._uploadFile)
 }
