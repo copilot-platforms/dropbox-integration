9bdb3ee0b780eaf1b01ff3eb48caeea360bdf2c7
arpandhakal <arpandhakal20@gmail.com>
2025-12-09 13:55:03 +0545

    fix(OUT-2772): disable app bridge buttons when app is disconnected in real time

--- src/components/layouts/CheckConnection.tsx
+++ src/components/layouts/CheckConnection.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { useAuthContext } from '@/features/auth/hooks/useAuth'
+import { postMessageParentDashboard } from '@/lib/copilot/hooks/app-bridge/postParentMessage'
 
 export const CheckConnection = ({ children }: { children: React.ReactNode }) => {
   const { connectionStatus } = useAuthContext()
-  if (!connectionStatus) return
+  if (!connectionStatus) {
+    postMessageParentDashboard({ type: 'header.actionsMenu', items: [] })
+    postMessageParentDashboard({ type: 'header.primaryCta' })
+    return
+  } //remove app bridge functionality when app is disconnected.
 
   return <>{children}</>
 }
