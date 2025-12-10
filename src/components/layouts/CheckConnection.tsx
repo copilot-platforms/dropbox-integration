e7b0c17a165d37fe8fefe8c1f25872eb66af8a74
SandipBajracharya <sndp106.sb@gmail.com>
2025-12-10 13:42:31 +0545

    feat(OUT-2705): improve initial page
    - [x] add callout along with button to connect to Dropbox
    - [x] show table and add overlay over it before connection

--- src/components/layouts/CheckConnection.tsx
+++ src/components/layouts/CheckConnection.tsx
@@ -1,15 +1,18 @@
 'use client'
 
 import { useAuthContext } from '@/features/auth/hooks/useAuth'
-import { postMessageParentDashboard } from '@/lib/copilot/hooks/app-bridge/postParentMessage'
+import { Overlay } from '@/features/sync/components/Overlay'
 
 export const CheckConnection = ({ children }: { children: React.ReactNode }) => {
   const { connectionStatus } = useAuthContext()
   if (!connectionStatus) {
-    postMessageParentDashboard({ type: 'header.actionsMenu', items: [] })
-    postMessageParentDashboard({ type: 'header.primaryCta' })
-    return
-  } //remove app bridge functionality when app is disconnected.
+    return (
+      <div className="relative opacity-50">
+        <Overlay />
+        {children}
+      </div>
+    )
+  }
 
   return <>{children}</>
 }
