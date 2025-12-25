6cdbafe0d3f123eee111b8326ad998a8c36a9c9d
SandipBajracharya <sndp106.sb@gmail.com>
2025-12-25 14:16:30 +0545

    docs(OUT-2842): add note to create react portal if the UI responsive is not fixed

--- src/components/ui/CopilotSelector.tsx
+++ src/components/ui/CopilotSelector.tsx
@@ -19,6 +19,8 @@ export const CopilotSelector = ({
 }: CopilotSelectorProps) => {
   if (typeof window !== 'undefined')
     return (
+      // Note: to fix the UI responsive completely, need to create a custom react portal and wrap this component with it.
+      // Since we are using the same input field given by this selector component for now, need to create a separate input field for this implementation
       <div className="w-64">
         <UserCompanySelector
           name={name}
