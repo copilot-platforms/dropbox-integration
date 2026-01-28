c4f7995199412439d985b94fd999ce849e570850
SandipBajracharya <sndp106.sb@gmail.com>
2026-01-28 19:43:40 +0545

    feat(OUT-3023): list child folders of searched term in dropbox dropdown component
    - [x] update logic to search for child folders
    - [x] for team folders, re-instantiate the dropbox sdk with that folder as root
    - [x] return all child folders in flat array
    - [x] disable the synced folders in UI table

--- src/db/constants.ts
+++ src/db/constants.ts
@@ -3,3 +3,9 @@ export enum ObjectType {
   FOLDER = 'folder',
 }
 export type ObjectTypeValue = (typeof ObjectType)[keyof typeof ObjectType]
+
+export enum DropboxClientType {
+  ROOT = 'root',
+  NAMESPACE_ID = 'namespace_id',
+}
+export type DropboxClientTypeValue = (typeof DropboxClientType)[keyof typeof DropboxClientType]
