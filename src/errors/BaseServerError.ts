b95f1ddcb85a916d48f7e70c1f16a0a4e1089f57
arpandhakal <arpandhakal20@gmail.com>
2026-01-30 17:01:41 +0545

    fix(OUT-3019): applied waiting for retry after value from dropbox api header response

--- src/errors/BaseServerError.ts
+++ src/errors/BaseServerError.ts
@@ -1,24 +1,28 @@
 export interface StatusableError extends Error {
   status: number
+  retryAfter?: number
 }
 
 /**
  * Base error class for Server components / actions / API routes
  */
 export class BaseServerError extends Error {
+  public retryAfter?: number
   constructor(
     message: string,
     public readonly statusCode: number,
+    retryAfter?: number,
   ) {
     super(message)
     this.name = 'BaseServerError'
+    this.retryAfter = retryAfter
   }
 }
 
 export const baseServerErrorFactory = (name: string, message: string, statusCode: number) => {
   return class extends BaseServerError {
-    constructor(messageOverride?: string) {
-      super(messageOverride || message, statusCode)
+    constructor(messageOverride?: string, retryAfter?: number) {
+      super(messageOverride || message, statusCode, retryAfter)
       this.name = name
     }
   }
