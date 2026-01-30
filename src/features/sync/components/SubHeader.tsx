28781f917458fafabd711c90ff3125fa24f7a1fe
SandipBajracharya <sndp106.sb@gmail.com>
2026-01-30 17:35:28 +0545

    test(OUT-3032): include add button in the page

--- src/features/sync/components/SubHeader.tsx
+++ src/features/sync/components/SubHeader.tsx
@@ -1,9 +1,15 @@
 'use client'
 
+import { Button } from 'copilot-design-system'
+import { useSubHeader } from '@/features/sync/hooks/useSubHeader'
+
 export const SubHeader = () => {
+  const { handleAddRule } = useSubHeader()
+
   return (
     <div className="mx-10 mb-5 flex items-center justify-between">
       <div className="font-bold text-lg">Sync Rules</div>
+      <Button label="Add" prefixIcon="Plus" onClick={handleAddRule} />
     </div>
   )
 }
