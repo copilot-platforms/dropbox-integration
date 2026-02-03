9f798ee448b1bed2d5be622811afde983786c5ab
arpandhakal <arpandhakal20@gmail.com>
2026-02-03 17:05:52 +0545

    fix(OUT-3046): finalized migrations

--- src/db/migrations/20260203102948_add-uq-to-channel-sync.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS "uq_channel_sync_channel_id_dbx_root_path";
-
-CREATE UNIQUE INDEX "uq_channel_sync__channel_id_dbx_root_path"
-ON "public"."channel_sync" ("assembly_channel_id", "dbx_root_path")
-WHERE "deleted_at" IS NULL;
---
+++ src/db/migrations/20260203111950_add-uq-to-channel-sync.sql
@@ -0,0 +1,2 @@
+DROP INDEX "uq_channel_sync_channel_id_dbx_root_path";
+CREATE UNIQUE INDEX "uq_channel_sync__channel_id_dbx_root_path" ON "channel_sync" USING btree ("assembly_channel_id","dbx_root_path") WHERE "channel_sync"."deleted_at" is null;
