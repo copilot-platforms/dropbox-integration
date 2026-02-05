b4a48ba46d0ce3abf89292afdfaf08dbbd5d1d67
arpandhakal <arpandhakal20@gmail.com>
2026-02-05 18:24:46 +0545

    fix(OUT-3065): added an api to resync files failed to upload on assembly

---
+++ src/features/workers/resync-failed-files/api/resync-failed-files.controller.ts
@@ -0,0 +1,15 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import env from '@/config/server.env'
+import APIError from '@/errors/APIError'
+import { ResyncService } from '@/features/workers/resync-failed-files/lib/resync-failed-files.service'
+
+export const resyncFailedFiles = async (request: NextRequest) => {
+  const authHeader = request.headers.get('authorization')
+  if (authHeader !== `Bearer ${env.CRON_SECRET}`) {
+    throw new APIError('Unauthorized', 401)
+  }
+
+  const resyncService = new ResyncService()
+  await resyncService.resyncFailedFiles()
+  return NextResponse.json({ message: 'Succeslyfully trigger re syncing of files.' })
+}
